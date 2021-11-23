0819ecc3defa8e777846b2391416d1f77b011a44
Marco Selva <39164743+marcoselva@users.noreply.github.com>
2021-11-23 18:14:27 +0100

    Revert "Add new file loader  functionality (#1)"
    This reverts commit cc663fd9000fdbae84464f143384b25a91aa0296.

--- code/routine.py
+++ code/routine.py
@@ -1,23 +1,64 @@
-import logging
-
 import main
-from loader import load_raw_files
-from settings import PathConfig
+import os
+import time
 
-__author__ = "Daniel Ndegwa, MARCO SELVA"
-__credits__ = ["Daniel Ndegwa, MARCO SELVA"]
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-# the path were al files are extract, you have to separate file extracted by sport and by type of data
-workPath = PathConfig.WORK_DIR
-raw_data_files = load_raw_files()
-data_ADVANCED_TENNIS = raw_data_files["ADVANCED"]["TENNIS"]
+# the path were al files are extracte, you have to sperate file extracted by sport and by type of data
+workPath = 'D:/00_PROJECTs/40_betfair/rawDataConversion/rawDataConversion/code/rawInput/'
 
-logging.info("[*] starting the routine")
+# the path were i will place the file to be converted
+data_ADVANCED_SOCCER = 'D:/00_PROJECTs/40_betfair/rawDataConversion/rawDataConversion/rawData/ADVANCED/SOCCER/'
+data_ADVANCED_TENNIS = 'D:/00_PROJECTs/40_betfair/rawDataConversion/rawDataConversion/rawData/ADVANCED/TENNIS/'
+data_ADVANCED_HORSE_RACING = 'D:/00_PROJECTs/40_betfair/rawDataConversion/rawDataConversion/rawData/ADVANCED/HORSE RACING/'
 
-# main.main(str(data_ADVANCED_TENNIS), str(workPath), 'ADVANCED')
-main.single_sport(data_ADVANCED_TENNIS, "TENNIS", 'ADVANCED')
+data_BASIC_SOCCER = 'D:/00_PROJECTs/40_betfair/rawDataConversion/rawDataConversion/rawData/BASIC/SOCCER/'
+data_BASIC_TENNIS = 'D:/00_PROJECTs/40_betfair/rawDataConversion/rawDataConversion/rawData/BASIC/TENNIS/'
+data_BASIC_HORSE_RACING = 'D:/00_PROJECTs/40_betfair/rawDataConversion/rawDataConversion/rawData/BASIC/HORSE RACING/'
 
-logging.info(' -----  END ADVANCED ----')
-logging.info("[*] finished the routine")
+
+# have to run for all above folder
+
+# start the routine
+main.main(data_ADVANCED_TENNIS,workPath,'ADVANCED')
+
+
+print(' -----  END ADVANCED ----')
+
+"""
+# EMPTY FOLDER
+for filename in os.listdir(mainPath):
+    file_path = os.path.join(mainPath, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            mainPath.rmtree(file_path)
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+
+print(' -----  EMPTY ----')
+time.sleep(3)
+print('\n\n\n\n -----  START ADVANCED ----')
+main.main(data_BASIC,mainPath,'BASIC')
+
+# EMPTY FOLDER
+for filename in os.listdir(mainPath):
+    file_path = os.path.join(mainPath, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            mainPath.rmtree(file_path)
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+
+print(' -----  EMPTY ----')
+time.sleep(3)
+
+
+
+print(' -----  END ----')
+"""
--- code/utils.py
+++ code/utils.py
@@ -1,20 +1,21 @@
 import json
 from datetime import datetime
-
-__author__ = "Daniel Ndegwa, MARCO SELVA"
-__credits__ = ["Daniel Ndegwa, MARCO SELVA"]
+import pprint
 
 
 # save market (dict) as JSON in path
 def saveMarketJSON(file):
     print("Saving MARKET " + file.info['name'] + " in " + './exportOutput/market/' + file.info['id'] + '.json')
 
-    # it doesn't actualy works but here you have to separate by sport the output like /exportOutput/market/TENNIS, /exportOutput/market/SOCCER, /exportOutput/market/HORSE RACING
+
+
+
+    #it doesn't actualy works but here you have to separate by sport the output like /exportOutput/market/TENNIS, /exportOutput/market/SOCCER, /exportOutput/market/HORSE RACING
     t = int(file.info['openDate'] / 1000)
     time = datetime.utcfromtimestamp(t).strftime('%Y_%m_%d')
 
     # for the check if isnt preset / in file names
-    path = './exportOutput/markets/' + time + '_' + file.info['name'] + '_' + file.info['id'] + '.json'
+    path = './exportOutput/markets/' + time  + '_' + file.info['name'] + '_' +  file.info['id'] + '.json'
 
     with open(path, 'w') as outfile:
         json.dump(file.__dict__, outfile)
@@ -22,11 +23,13 @@ def saveMarketJSON(file):
 
 # save runners (list) as JSON in path
 
-# it should save the file with the date so when i upload in the DB i know what is the last runner file to upload
+#it should save the file with the date so when i upload in the DB i know what is the last runner file to upload
 def saveRunnersJSON(runnersDB):
+
     print('Saving RUNNERS DB in ./exportOutput/runners/runnersDB.json')
 
     path = './exportOutput/runners/runnersDB.json'
 
     with open(path, 'w') as outfile:
         json.dump(runnersDB.runners, outfile)
+
